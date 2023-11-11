519db73a7d2ad29d174ee9ac15fa11de827e7590
Martin Ferreira <martin.f.yic@gmail.com>
2023-11-11 12:04:32 -0300

    Refact: implement repository at main

--- src/main/java/com/sales/proyectocoder/ProyectocoderApplication.java
+++ src/main/java/com/sales/proyectocoder/ProyectocoderApplication.java
@@ -1,13 +1,31 @@
 package com.sales.proyectocoder;
 
+import com.sales.proyectocoder.repository.ClientRepository;
+import com.sales.proyectocoder.repository.InvoiceDetailRepository;
+import com.sales.proyectocoder.repository.InvoiceRepository;
+import com.sales.proyectocoder.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectocoderApplication {
+public class ProyectocoderApplication implements CommandLineRunner {
+
+	@Autowired
+	ClientRepository clientRepository;
+	@Autowired
+	ProductRepository productRepository;
+	@Autowired
+	InvoiceRepository invoiceRepository;
+	@Autowired
+	InvoiceDetailRepository invoiceDetailRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectocoderApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {}
 
 }
