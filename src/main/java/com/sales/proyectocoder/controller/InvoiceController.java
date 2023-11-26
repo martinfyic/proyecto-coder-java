217a2fa51cfa63bd9d7f91428449763bc59ed1c5
Martin Ferreira <martin.f.yic@gmail.com>
2023-11-26 16:46:38 -0300

    Refactor - add InvoiceController.getAllInvoices

--- src/main/java/com/sales/proyectocoder/controller/InvoiceController.java
+++ src/main/java/com/sales/proyectocoder/controller/InvoiceController.java
@@ -1,4 +1,36 @@
 package com.sales.proyectocoder.controller;
 
+import com.sales.proyectocoder.model.InvoiceModel;
+import com.sales.proyectocoder.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/invoices")
 public class InvoiceController {
+
+  @Autowired
+  private InvoiceService invoiceService;
+
+  /**
+   *
+   * @return Lista de todas las invoices
+   */
+  @GetMapping
+  public ResponseEntity<List<InvoiceModel>> getAllInvoices() {
+    List<InvoiceModel> invoices = invoiceService.getAllInvoices();
+    return ResponseEntity.ok(invoices);
+  }
+
+  /* TODO
+  *   POST - Crear Invoice
+  */
+
+  /* TODO
+   *   GET - Listar invoice por ID
+   */
+
 }
