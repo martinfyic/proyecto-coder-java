18eed542bdca7e3cd34051742b70b096ad645758
Martin Ferreira <martin.f.yic@gmail.com>
2023-11-27 22:29:44 -0300

    refactor: delete validations at ClientModel

--- src/main/java/com/sales/proyectocoder/model/ClientModel.java
+++ src/main/java/com/sales/proyectocoder/model/ClientModel.java
@@ -1,8 +1,6 @@
 package com.sales.proyectocoder.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Date;
@@ -16,19 +14,15 @@ public class ClientModel {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
-  @NotBlank
   @Column(name = "first_name", nullable = false, length = 75)
   private String firstName;
 
-  @NotBlank
   @Column(name = "last_name", nullable = false, length = 75)
   private String lastName;
 
-  @NotNull
   @Column(nullable = false)
   private Date birthdate;
 
-  @NotBlank
   @Column(name = "doc_number", nullable = false, length = 11)
   private String docNumber;
 
