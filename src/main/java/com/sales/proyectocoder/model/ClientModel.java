7bd77918765ac0f1c225b4dad2c74272e69561a2
Martin Ferreira <martin.f.yic@gmail.com>
2023-12-02 19:09:12 -0300

    add documentation with swagger

--- src/main/java/com/sales/proyectocoder/model/ClientModel.java
+++ src/main/java/com/sales/proyectocoder/model/ClientModel.java
@@ -1,5 +1,6 @@
 package com.sales.proyectocoder.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "clients")
+@Schema(description = "Modelo de clientes")
 public class ClientModel {
 
   @Id
