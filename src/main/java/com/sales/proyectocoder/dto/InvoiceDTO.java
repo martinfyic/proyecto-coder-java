4373ce4ee4a5179e5ff74c36b67c619f0a1c769c
Martin Ferreira <martin.f.yic@gmail.com>
2023-12-03 13:11:36 -0300

    add relation tables invoiceDetail

--- src/main/java/com/sales/proyectocoder/dto/InvoiceDTO.java
+++ src/main/java/com/sales/proyectocoder/dto/InvoiceDTO.java
@@ -1,9 +1,11 @@
-package com.sales.proyectocoder.DTO;
+package com.sales.proyectocoder.dto;
 
+import com.sales.proyectocoder.model.InvoiceDetailModel;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.Set;
 
 @Data
 public class InvoiceDTO {
@@ -16,4 +18,6 @@ public class InvoiceDTO {
 
   @NotNull
   private Double total;
+
+  private Set<InvoiceDetailModel> invoiceDetail;
 }
