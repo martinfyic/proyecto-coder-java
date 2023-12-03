12e875736c879e2e8ad0bdd6367d9bd6a520dddb
Martin Ferreira <martin.f.yic@gmail.com>
2023-12-03 13:11:51 -0300

    add relation tables invoiceDetail

--- src/main/java/com/sales/proyectocoder/model/InvoiceModel.java
+++ src/main/java/com/sales/proyectocoder/model/InvoiceModel.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Data
@@ -14,9 +15,12 @@ public class InvoiceModel {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
-  @ManyToOne
+  @ManyToOne(cascade = CascadeType.ALL)
   @JoinColumn(name = "client_id", nullable = false)
   private ClientModel client;
+
+  @OneToMany(mappedBy = "invoice",cascade = CascadeType.ALL)
+  private Set<InvoiceDetailModel> invoiceDetails;
 
   @Column(name = "created_at", nullable = false)
   @Temporal(TemporalType.TIMESTAMP)
