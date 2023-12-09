41c4543f1ce022b474190e710c052b83f8720209
Martin Ferreira <martin.f.yic@gmail.com>
2023-12-09 10:30:56 -0300

    fix add id at InvoiceDTO

--- src/main/java/com/sales/proyectocoder/dto/InvoiceDTO.java
+++ src/main/java/com/sales/proyectocoder/dto/InvoiceDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 @EqualsAndHashCode
 public class InvoiceDTO {
 
+  private Integer id;
   private Integer clientId;
   private LocalDateTime createdAt;
   private List<InvoiceDetailDTO> details;
