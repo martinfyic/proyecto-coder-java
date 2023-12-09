7018bc7cda9d4b1364f39c755e0f739c5554de2a
Martin Ferreira <martin.f.yic@gmail.com>
2023-12-09 09:29:15 -0300

    add EntityNotFoundException handleexception

---
+++ src/main/java/com/sales/proyectocoder/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sales.proyectocoder.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+}
