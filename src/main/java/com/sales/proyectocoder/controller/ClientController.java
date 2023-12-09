164d811bb252404a7c7f103d5e8158b2d3a55052
Martin Ferreira <martin.f.yic@gmail.com>
2023-12-08 23:13:13 -0300

    Refactor add swagger

--- src/main/java/com/sales/proyectocoder/controller/ClientController.java
+++ src/main/java/com/sales/proyectocoder/controller/ClientController.java
@@ -1,6 +1,5 @@
 package com.sales.proyectocoder.controller;
 
-import com.sales.proyectocoder.dto.ClientDTO;
 import com.sales.proyectocoder.model.ClientModel;
 import com.sales.proyectocoder.response.ClientYearsOldResponse;
 import com.sales.proyectocoder.response.DeleteResponse;
@@ -20,33 +19,25 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/clients")
-@Tag(name="Controlador Client", description = "Request relacionadas con los usuarios")
+@Tag(name = "Client Controller", description = "Request relacionadas con los usuarios")
 public class ClientController {
   @Autowired
   private ClientService clientService;
 
-  /**
-   * @return Lista de todos los clientes
-   */
-  @Operation(summary = "Listar todos los usuarios", description = "Listar todos los usuarios en formato string")
+  @Operation(summary = "Lista todos los usuarios", description = "Lista todos los usuarios")
   @GetMapping
-  public ResponseEntity<List<ClientDTO>> getAllClients() {
-    List<ClientDTO> clients = clientService.getAllClients();
+  public ResponseEntity<List<ClientModel>> getAllClients() {
+    List<ClientModel> clients = clientService.getAllClients();
     return ResponseEntity.ok(clients);
   }
 
-  /**
-   *
-   * @param id numero identificador del cliente
-   * @return Retorna cliente seleccionado por su id
-   */
-  @Operation(summary = "Muestra uns usuario", description = "Muestra un usuario por su id")
+  @Operation(summary = "Muestra un usuario", description = "Muestra un usuario por su id")
   @GetMapping("/{id}")
   public ResponseEntity<?> getClientById(@PathVariable Integer id) {
-    ClientDTO clientById = clientService.getClientById(id);
+    ClientModel client = clientService.getClientById(id);
 
-    if (clientById != null) {
-      return ResponseEntity.ok(clientById);
+    if (client != null) {
+      return ResponseEntity.ok(client);
     } else {
       Map<String, String> response = new HashMap<>();
       response.put("status", "error");
@@ -56,16 +47,8 @@ public class ClientController {
 
   }
 
-  /**
-   *
-   * @param id numero identificador del cliente
-   * @return Retorna la edad de un cliente seleccionado
-   */
-  @Operation(summary = "Muestra un usuario para obtener su edad", description = "Muestra un usuario por su id y obtiene la edad")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Request Exitoso"),
-      @ApiResponse(responseCode = "400", description = "Bad Request")
-  })
+  @Operation(summary = "Muestra un usuario para con el calculo de su edad", description = "Muestra un usuario por su id y obtiene la edad")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Request Exitoso"), @ApiResponse(responseCode = "400", description = "Bad Request")})
   @GetMapping("/{id}/years-old")
   public ResponseEntity<?> getClientYearsOld(@PathVariable Integer id) {
     ClientYearsOldResponse clientResponse = clientService.getClientYearsOld(id);
@@ -80,37 +63,24 @@ public class ClientController {
     }
   }
 
-  /**
-   *
-   * @param clientDTO información del cliente a crear, ejemplo: { "firstName": "Bruce ", "lastName": "Wayne ", "birthdate": "2005-10-04", "docNumber": "25643456" }
-   * @return Crea cliente creado
-   */
   @Operation(summary = "Crear un usuario", description = "Crea un usuario y lo guarda en DB")
   @PostMapping
-  public ResponseEntity<ClientModel> createClient(@RequestBody ClientDTO clientDTO) {
-    ClientModel createdClient = clientService.createClient(clientDTO);
+  public ResponseEntity<ClientModel> createClient(@RequestBody ClientModel client) {
+    ClientModel createdClient = clientService.createClient(client);
     return new ResponseEntity<>(createdClient, HttpStatus.CREATED);
   }
 
-  /**
-   * @param client información del cliente que vamos a actualizar, ejemplo: { "firstName": "Bruce ", "lastName": "Wayne ", "birthdate": "2005-10-04", "docNumber": "25643456" }
-   * @param id     identificador unico del cliente que vamos a actualizar
-   * @return Actualiza un cliente seleccionado
-   */
+  @Operation(summary = "Actualiza usuario", description = "Actualiza un usuario buscado por su id")
   @PutMapping("/{id}")
   public ResponseEntity<ClientModel> updateClient(@RequestBody ClientModel client, @PathVariable Integer id) {
     ClientModel updatedClient = clientService.updateClient(client, id);
     return (updatedClient != null) ? ResponseEntity.ok(updatedClient) : ResponseEntity.notFound().build();
   }
 
-  /**
-   *
-   * @param id numero identificador del cliente
-   * @return Elimina un cliente seleccionado
-   */
+  @Operation(summary = "Elimina un usuario por su id", description = "Elimina un usuario de la DB por su id")
   @DeleteMapping("/{id}")
   public ResponseEntity<DeleteResponse> deleteClient(@PathVariable Integer id) {
-    DeleteResponse  response = clientService.deleteClient(id);
+    DeleteResponse response = clientService.deleteClient(id);
     return ResponseEntity.ok(response);
   }
 }
